71ceac2602be7c06bd24cfa37790e992a0080734
yaoyao_ing <131286351+yaoyao-ing@users.noreply.github.com>
2024-02-03 19:56:13 +0800

    Add files via upload

---
+++ test2_1.js
@@ -0,0 +1,25 @@
+let QQUser = {
+    name: "QQ",
+    age: 20,
+    birthday: "2003-12-12",
+    hobby: "eating",
+    gender: "male",
+    school: "cqupt",
+    status: "online",
+    QQnumber: "1589453906",
+    QQlevel: 15,
+    friendsNumber: 28,
+    groupsNumber: 8,
+
+};
+console.log(QQUser.name);
+console.log(QQUser.age);
+console.log(QQUser.birthday);
+console.log(QQUser.hobby);
+console.log(QQUser.gender);
+console.log(QQUser.school);
+console.log(QQUser.status);
+console.log(QQUser.QQnumber);
+console.log(QQUser.QQlevel);
+console.log(QQUser.friendsNumber);
+console.log(QQUser.groupsNumber); 
